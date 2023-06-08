5752b08f1259933e91c4eecf44e95afae1c8983a
Joao Oliveira <jaoovit@gmail.com>
2023-06-08 16:11:48 +0100

    install axios

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,12 +1,18 @@
 // @flow 
 import * as React from 'react';
+import axios from 'axios';
 type Props = {
     
 };
 export const Room = (props: Props) => {
+    axios.get('http://localhost:3000/rooms')
     return (
         <div>
-            Rooms
+            <h1>Rooms</h1>
+            <ul>
+                <li>Room 1</li>
+                <li>Room 2</li>
+            </ul>
         </div>
     );
 };
